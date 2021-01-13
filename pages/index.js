b41c1636ebddb2f69d24fdde35025ab39edd6d74
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-13 14:52:51 +0100

    crypto map commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
 
 export default function Home({ res }) {
   console.log(res)
   return (
       <Layout page="Crypto Watch - Accueil" >
-        
+        <ul className="flex flex-wrap justify-around py-10">
+          {res.map((crypto, index) => (
+            <li 
+            key={index} 
+            className="relative hover:shadow-md p-8 border border-blue-300 rounded-3xl bg-blue-100 md:w-auto flex-1 mx-5"
+            >
+              <Link href="/"> 
+                <a className="rounded-md">
+                  <div className="text-center">
+                    <img 
+                    src={crypto.logo_url} 
+                    alt={crypto.name} 
+                    className="w-20 h-20 mx-auto mb-6" 
+                    />
+                  </div>
+                  <h2 className="text-2xl mb-6 uppercase tracking-wider">
+                    {crypto.name}
+                  </h2>
+                  <h3 className="font-bold text-2xl mb-4">
+                    {crypto.price} €
+                  </h3>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </Layout>
   )
 }
@@ -19,6 +45,6 @@ export async function getStaticProps(context) {
       props: { res }
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 }
