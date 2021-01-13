96ae0df4f3900ce6a8c7a436f9b061567db82226
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-13 15:52:19 +0100

    currency commit

--- pages/index.js
+++ pages/index.js
@@ -27,14 +27,37 @@ export default function Home({ res }) {
                   <h3 className="font-bold text-2xl mb-4">
                     {parseFloat(crypto.price).toFixed(2)} €
                   </h3>
+
                   <p>
                     1 jour : {""}
                     <span className="font-bold">
                       {parseFloat(crypto['1d'].price_change_pct * 100).toFixed(2) + "%"}
                     </span>{crypto["1d"].price_change_pct < 0 ? (
-                      <span className="text-red-500">&#x2798;</span>
+                      <span className="text-red-500"> &#x2798;</span>
                     ) : (
-                      <span className="text-green-500">&#x279A;</span>
+                      <span className="text-green-500"> &#x279A;</span>
+                    )}
+                  </p>
+
+                  <p>
+                    1 mois : {""}
+                    <span className="font-bold">
+                      {parseFloat(crypto['30d'].price_change_pct * 100).toFixed(2) + "%"}
+                    </span>{crypto["30d"].price_change_pct < 0 ? (
+                      <span className="text-red-500"> &#x2798;</span>
+                    ) : (
+                      <span className="text-green-500"> &#x279A;</span>
+                    )}
+                  </p>
+
+                  <p>
+                    1 an : {""}
+                    <span className="font-bold">
+                      {parseFloat(crypto['365d'].price_change_pct * 100).toFixed(2) + "%"}
+                    </span>{crypto["365d"].price_change_pct < 0 ? (
+                      <span className="text-red-500"> &#x2798;</span>
+                    ) : (
+                      <span className="text-green-500"> &#x279A;</span>
                     )}
                   </p>
                 </a>
