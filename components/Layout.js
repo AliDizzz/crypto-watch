5405d615a1fcf2e69f976feafcdc04d51c06b8c5
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-13 10:38:06 +0100

    layout button commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Layout({ children, page }) {
     return (
@@ -9,7 +10,16 @@ export default function Layout({ children, page }) {
                 <header className="container-lg">
                     <h1 className="text-5xl mb-2">CRYPTO WATCH</h1>
                     <div className="inline-grid grid-cols-2 gap-x-10 p-4">
-
+                        <Link rel="stylesheet" href="/">
+                            <button className="bg-blue-200 p-3 m-2 rounded-3xl hover:shadow-md border-2 border-blue-300">
+                                Accueil
+                            </button>
+                        </Link>
+                        <Link rel="stylesheet" href="/about">
+                            <button className="bg-blue-200 p-3 m-2 rounded-3xl hover:shadow-md border-2 border-blue-300">
+                                À propos
+                            </button>
+                        </Link>
                     </div>
                 </header>
         </div>
