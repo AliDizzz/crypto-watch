5258106fe91577d3a5d5a75a1af13977b61bed99
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-13 15:40:50 +0100

    crypto card  commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ res }) {
           {res.map((crypto, index) => (
             <li 
             key={index} 
-            className="relative hover:shadow-md p-8 border border-blue-300 rounded-3xl bg-blue-100 md:w-auto flex-1 mx-5"
+            className="relative hover:shadow-md p-8 border border-blue-300 rounded-3xl bg-blue-100 md:w-auto flex-1 my-5 mx-5"
             >
               <Link href="/"> 
                 <a className="rounded-md">
@@ -21,11 +21,22 @@ export default function Home({ res }) {
                     />
                   </div>
                   <h2 className="text-2xl mb-6 uppercase tracking-wider">
-                    {crypto.name}
+                    {crypto.name} 
                   </h2>
+                  <h3>Rang actuel {crypto.rank}</h3>
                   <h3 className="font-bold text-2xl mb-4">
-                    {crypto.price} €
+                    {parseFloat(crypto.price).toFixed(2)} €
                   </h3>
+                  <p>
+                    1 jour : {""}
+                    <span className="font-bold">
+                      {parseFloat(crypto['1d'].price_change_pct * 100).toFixed(2) + "%"}
+                    </span>{crypto["1d"].price_change_pct < 0 ? (
+                      <span className="text-red-500">&#x2798;</span>
+                    ) : (
+                      <span className="text-green-500">&#x279A;</span>
+                    )}
+                  </p>
                 </a>
               </Link>
             </li>
@@ -39,7 +50,7 @@ export default function Home({ res }) {
 export async function getStaticProps(context) {
   try {
     const res = await fetch(
-      "https://api.nomics.com/v1/currencies/ticker?key=2a131e8732f83eebe345a11e64b7f52d&ids=BTC,ETH,XRP,AAVE,LTC,3XT,BCH,ADA,BOR&interval=1d,30d,365d&convert=EUR"
+      "https://api.nomics.com/v1/currencies/ticker?key=2a131e8732f83eebe345a11e64b7f52d&ids=BTC,ETH,XRP,ADA,LTC,LINK,BCH,DOT,XLM,USDT&interval=1d,30d,365d&convert=EUR"
     ).then((res) => res.json());
     return {
       props: { res }
