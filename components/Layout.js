18b01e80dd9a7a62663dbd92b8db0fa14eb32560
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-13 10:16:33 +0100

    layout commit

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from "next/head";
+
+export default function Layout({ children, page}) {
+    return (
+        <div className="bg-blue-50 pt-5 text-center min-h-screen">
+            <Head>
+                <title>{page}</title>
+            </Head>
+                {children}
+        </div>
+    )
+}
